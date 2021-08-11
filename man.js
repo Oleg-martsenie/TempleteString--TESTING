581b2e41851710809ef71ded463cf729ab355421
Gabriel Póvoa <povoajoao6@gmail.com>
2021-08-11 13:08:05 -0300

    Studying about

---
+++ man.js
@@ -0,0 +1,6 @@
+let name = 'Gabriel Póvoa';
+let age = 19;
+
+let sentence = `It's ${name}, I'm ${age}, in the next year I'll be ${age + 1}`;
+
+document.querySelector('.demo').innerHTML = sentence;
